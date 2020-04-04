4cc1defa7904e07377be29ee5836324ce5d49341
David Mallows <dave.mallows@gmail.com>
2020-04-04 18:56:06 +0200

    Additional analysis

--- fetch.sh
+++ fetch.sh
@@ -4,7 +4,7 @@ curl -O https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_co
 file=time_series_covid19_deaths_global.csv
 
 head -n 1 $file > selected.csv
-grep '^,US' $file >> selected.csv
+# grep '^,US' $file >> selected.csv
 grep '^,United Kingdom' $file >> selected.csv
 grep '^,France' $file >> selected.csv
 grep '^,Spain' $file >> selected.csv
